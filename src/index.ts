20bdca7d61299a1185eaa672e3da56e00ece8e8e
Andrew McMullin <48926044+mcmullinboy15@users.noreply.github.com>
2026-08-16 20:25:00 -0600

    export compiler and production APIs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-export { defineSchema, t, type Column, type SchemaDef, type TableDef } from "./schema.ts";
-export { createXql, type Adapter, type Query, type Xql } from "./xql.ts";
+export {
+  castZod,
+  column,
+  defineSchema,
+  t,
+  type Codec,
+  type Column,
+  type SchemaDef,
+  type TableDef,
+} from "./schema.ts";
+export {
+  createXql,
+  type Adapter,
+  type AdapterQueryContext,
+  type CompiledXql,
+  type Query,
+  type QueryExecutionOptions,
+  type QueryResult,
+  type Xql,
+  type XqlCompiledQueries,
+  type XqlOptions,
+} from "./xql.ts";
+export type {
+  CompiledPgType,
+  CompiledQueryColumn,
+  CompiledQueryDescriptor,
+  CompiledQueryManifest,
+  CompiledQueryParam,
+  CompiledRuntimeOptions,
+} from "./compiled.ts";
 export { XqlError as XqlRuntimeError } from "./runtime/parse.ts";
 export type { XqlError } from "./type/select.ts";
 export type { ParamsOfQuery, RowOfQuery, SqlValue } from "./type/query.ts";
